0c5d5f91436c5f428fe18c2e4e9f61c312ff00e9
Leticia Malipense <leticia.malipense@gmail.com>
2020-07-12 16:52:27 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.ifsp.scl.programacaoconcorrenteui
+
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+
+class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines)
+    }
+
+    override fun run() {
+
+    }
+}
