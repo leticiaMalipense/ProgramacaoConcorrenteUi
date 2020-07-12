d498400e4b2539c3c1acbe4d2e00daa37b1d8ced
Leticia Malipense <leticia.malipense@gmail.com>
2020-07-12 19:05:39 -0300

    Adicionando implementacao coroutines thread

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -2,15 +2,22 @@ package br.edu.ifsp.scl.programacaoconcorrenteui
 
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+import kotlinx.coroutines.*
 
 class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutines)
+        supportActionBar?.title = "CoroutinesThread"
     }
 
     override fun run() {
+        GlobalScope.launch(Dispatchers.Main) {
+            println("Kotlin Coroutines World!")
+            altaIv.setImageResource(R.drawable.android_preto)
+            baixaIv.setImageResource(R.drawable.android_verde)
+        }
 
     }
+
 }
